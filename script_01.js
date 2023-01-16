cb93599b7f290f0b1ba494228ee9bfc0a2be2c5a
Gazelle2010 <martin-schwerdtner@t-online.de>
2023-01-16 11:45:05 +0100

    Entscheidungsstrukturen Update

--- script_01.js
+++ script_01.js
@@ -4,8 +4,19 @@
 
 // Deklaration + Assigment
 const ageJohn = 35;
-const ageMark = 30;
+const ageMark = 35;
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
 
